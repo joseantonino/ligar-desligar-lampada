6a21ada341d202add2800338f5dd7ec11693e6c2
joseantonino <joseantonino78@gmail.com>
2025-04-02 21:02:25 -0300

    projeto ligar-desligar-lâmpada

---
+++ script.js
@@ -0,0 +1,9 @@
+function on() {
+  document.querySelector(".imagem").innerHTML =
+    '<img src="imagem/on.jpg" alt="">';
+}
+
+function off() {
+  document.querySelector(".imagem").innerHTML =
+    '<img src="imagem/off.png" alt="">';
+}
